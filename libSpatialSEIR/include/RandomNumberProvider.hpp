1ca18c342257a553a732dca1700f724e6b7752b3
Grant Brown <grant.brown73@gmail.com>
2014-03-25 08:11:04 -0500

    Start defining RandomNumberProvider class. This will need to be generalized to compile against either boost or R/Rcpp - I'm starting with boost.
    Current plan is to generate random numbers in large quantities on the CPU and provide them via buffers to OpenCL, as well as use them in the CPU functions.

---
+++ libSpatialSEIR/include/RandomNumberProvider.hpp
@@ -0,0 +1,42 @@
+#ifndef SPATIALSEIR_INCLUDEFILES
+#define SPATIALSEIR_INCLUDEFILES
+
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<cstring>
+#include<vector>
+#endif
+
+// Include logic about boost availability needs 
+// to go here. The library should try to find boost, 
+// but fall back on R. Expandin the CMake scripts
+// should help with this by using the appropriate
+// FIND_BOOST macros and setting appropriate 
+// preprocessor directives. 
+
+#include<boost/random/mersenne_twister.hpp>
+#include<boost/random/uniform_real.hpp>
+#include <boost/random/gamma_distribution.hpp>
+
+
+namespace SpatialSEIR
+{
+    class RandomNumberProvider()
+    {
+        public:
+            //Methods
+            RandomNumberProvider(int seed);
+            double uniform();   
+            double* uniform(int n);
+            double gamma();
+            double* gamma(int n);
+            ~RandomNumberProvider();
+
+
+            //Attributes
+            boost::random::mt19337 generator;
+            boost::random::uniform_real_distribution<> unidist;
+            boost::random::gamma_distribution<> gammadist;
+    };
+}
