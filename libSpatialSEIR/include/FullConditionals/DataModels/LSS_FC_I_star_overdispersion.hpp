111c0ead64783c63a330086e8d15a26f1853c978
Grant Brown <grant.brown73@gmail.com>
2014-08-19 20:00:32 -0500

    Start defining first non-degenerate data model class.

---
+++ libSpatialSEIR/include/FullConditionals/DataModels/LSS_FC_I_star_overdispersion.hpp
@@ -0,0 +1,55 @@
+#include <LSS_FullConditional.hpp>
+
+#ifndef FULL_CONDITIONAL_I_STAR_OD_INC
+#define FULL_CONDITIONAL_I_STAR_OD_INC
+
+namespace SpatialSEIR
+{
+    class ModelContext;
+    class CompartmentalModelMatrix;
+    class CovariateMatrix;
+    class OCLProvider;
+
+    /**
+     * FC_I_Star_overdispersed gives the overdispersion data model for I_star. 
+     */
+    class FC_I_Star_overdispersed : public CompartmentFullConditional
+    {
+        public:
+            FC_I_Star_overdispersed(ModelContext * _context,
+                                    int* _Y,
+                                    CompartmentalModelMatrix *_I_star, 
+                                    CompartmentalModelMatrix *_I, 
+                                    CompartmentalModelMatrix *_E,
+                                    CompartmentalModelMatrix *_R_star,
+                                    InitData *_A0,
+                                    double *_p_ei,
+                                    double *_p_ir,
+                                    double *_dispersion,
+                                    double _sliceWidth,
+                                    double _steadyStateConstraintPrecision);
+            virtual int evalCPU();
+            virtual int evalOCL();
+            virtual void sample(int verbose);
+            virtual long double getValue();
+            virtual void setValue(long double val);
+            virtual int calculateRelevantCompartments();
+            virtual int calculateRelevantCompartments_OCL();
+            virtual ~FC_I_Star_overdispersed();
+
+            ModelContext **context;
+            int** Y;
+            CompartmentalModelMatrix **I_star; 
+            CompartmentalModelMatrix **I; 
+            CompartmentalModelMatrix **E;
+            CompartmentalModelMatrix **R_star;
+            InitData **A0;
+            double **p_ei;
+            double **p_ir;
+            double **dispersion;
+            long double* value;
+            double* steadyStateConstraintPrecision;
+    };
+}
+
+#endif
