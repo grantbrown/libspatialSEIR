32a9fe3e248262dc055f66e23ae67648ff1cb74f
Brown <gown@lin1.public-health.uiowa.edu>
2014-08-21 22:29:09 -0500

    ostream tweaks

--- libSpatialSEIR/src/OCLProvider.cpp
+++ libSpatialSEIR/src/OCLProvider.cpp
@@ -178,8 +178,8 @@ SpatialSEIR::OCLProvider::OCLProvider()
     }
     catch(cl::Error e)
     {
-        cout << "Problem getting platforms:" << endl;
-        cout << e.what() << ": Error Code " << e.err() << endl;
+        cout << "Problem getting platforms:\n";
+        cout << e.what() << ": Error Code " << e.err() << "\n";
         throw(-1);
     }
 
