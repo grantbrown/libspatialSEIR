6cdbbd52b500ac0bc2fe1109d3288643836f31b9
Grant Brown <grant.brown73@gmail.com>
2014-12-05 15:09:12 -0600

    Make the convergence checker more robust

--- R/dev_package/R/checkConvergence.R
+++ R/dev_package/R/checkConvergence.R
@@ -3,16 +3,34 @@ checkConvergence = function(fileName1,fileName2,fileName3,maxVal=2,useUpper=FALS
     dat1 = read.csv(fileName1)
     dat2 = read.csv(fileName2)
     dat3 = read.csv(fileName3)
+    maxIdx = max(nrow(dat1), nrow(dat2), nrow(dat3))
+    dat1_sub = dat1[1:maxIdx,]
+    dat2_sub = dat2[1:maxIdx,]
+    dat3_sub = dat3[1:maxIdx,]
+
+
     
     iteration = dat1$Iteration
 
     varNames = names(dat1)
     lastVar = which(varNames == "gamma_ir") 
-    dat1 = as.mcmc(dat1[,1:lastVar])
-    dat2 = as.mcmc(dat2[,1:lastVar])
-    dat3 = as.mcmc(dat3[,1:lastVar])
-    mcl = mcmc.list(dat1,dat2,dat3)
-    diag = gelman.diag(mcl, multivariate=FALSE)
+    dat1_mc = as.mcmc(dat1_sub[,1:lastVar])
+    dat2_mc = as.mcmc(dat2_sub[,1:lastVar])
+    dat3_mc = as.mcmc(dat3_sub[,1:lastVar])
+    mcl = mcmc.list(dat1_mc,dat2_mc,dat3_mc)   
+
+    
+
+    diag = tryCatch({gelman.diag(mcl, multivariate=FALSE)}
+        , warning=function(w){return(FALSE)}
+        , error = function(e){return(FALSE)}
+    )
+    if (class(diag) == "logical" && !diag)
+    {
+        cat("WARNING: Convergence Check Failed. Continuing anyway...\n")
+        return(FALSE)
+    }
+
     if (useUpper)
     {
         criterion = max(diag[[1]][,2])
